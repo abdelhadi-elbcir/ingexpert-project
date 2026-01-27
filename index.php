c4057eab7c932b77637232c855829b9a9b32b301
Pomme12345 <entierement@laposte.net>
2026-01-27 15:43:32 +0100

    Update index.php

--- index.php
+++ index.php
@@ -53,7 +53,7 @@
       <h1>Conseil, formation, expertise <br>pour le <br>management de la Direction Technique dont la maintenance</h1>
 
       <div class="hero-subtitle">
-         <p><b></b>Audit, ingénierie, fiabilisation, achats, stock, formation, indicateurs, GMAO...b/b></p>
+         <p><b>Audit, ingénierie, fiabilisation, achats, stock, formation, indicateurs, GMAO...</b></p>
          <p>Accompagnement de tous les secteurs d’activité</p>
        
       </div>
